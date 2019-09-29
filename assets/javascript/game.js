14392e22f8a84e25bbdb6e979aae9b9c5edf6bb0
Cera Smart <cera_smart@yahoo.com>
2019-09-28 22:13:24 -0500

    added some CSS

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -10,20 +10,24 @@ var targetNumber = Math.floor(Math.random() * 100);
 
 $(".random-number").text(targetNumber);
 for (i = 0; i < 4; i++){
-  cupcakeNum.push(Math.floor(Math.random() * 15)+1);
+  cupcakeNum.push(Math.floor(Math.random() * 10)+1);
+
   $("img").attr("data-cupcakevalue", cupcakeNum[i]);
+  
 
 }
+
+
 
 }
 console.log(cupcakeNum)
 
 $('img').on("click", function(){
-    var cupcakeValue = ($(this).attr("date-cupcakevalue"));
+    var cupcakeValue = ($(this).attr("data-cupcakevalue"));
     cupcakeValue = parseInt(cupcakeValue);
 
     counter += cupcakeValue;
-
+    $(".total-score").text(counter);
     alert("New score: " + counter);
 
     if (counter === targetNumber) {
@@ -32,11 +36,13 @@ $('img').on("click", function(){
         $(".wins").text(wins);
         randomNumberGen();
       }
-  
+    
       else if (counter >= targetNumber) {
         alert("You lose!!");
         losses ++;
         $(".losses").text(losses);
         randomNumberGen();
       }
+
+    
 });
