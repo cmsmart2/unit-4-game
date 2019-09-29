7e14aa987b8c7c712589543daaa01e4ba8735ccf
Cera Smart <cera_smart@yahoo.com>
2019-09-29 11:33:25 -0500

    fixed the functionality so the game is now completely working

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,48 +1,79 @@
 var counter = 0;
-var cupcakeNum = [];
+var cupcakeOne =0;
+var cupcakeTwo =0;
+var cupcakeThree =0;
+var cupcakeFour =0;
 var wins = 0;
 var losses =0;
-randomNumberGen();
+var targetNumber=0;
 
 
-function randomNumberGen () {
-var targetNumber = Math.floor(Math.random() * 100);
-
-$(".random-number").text(targetNumber);
-for (i = 0; i < 4; i++){
-  cupcakeNum.push(Math.floor(Math.random() * 10)+1);
-
-  $("img").attr("data-cupcakevalue", cupcakeNum[i]);
+function randomNumGen (min,max){
+  return Math.floor(Math.random() *(max-min +1))+min;
   
+}
+var startGame = function () {
+    targetNumber = randomNumGen (15, 100)
+    $(".random-number").text(targetNumber);
+    counter = 0;
+    cupcakeOne = randomNumGen (1, 10)
+    cupcakeTwo = randomNumGen (1, 10)
+    cupcakeThree = randomNumGen (1, 10)
+    cupcakeFour = randomNumGen (1, 10)
+    console.log("One:" + cupcakeOne, "Two" + cupcakeTwo, "Three" + cupcakeThree, "Four"+ cupcakeFour );
 
 }
-
-
-
-}
-console.log(cupcakeNum)
-
-$('img').on("click", function(){
-    var cupcakeValue = ($(this).attr("data-cupcakevalue"));
-    cupcakeValue = parseInt(cupcakeValue);
-
-    counter += cupcakeValue;
-    $(".total-score").text(counter);
-    alert("New score: " + counter);
+var determineWinLose = function () {
 
     if (counter === targetNumber) {
         alert("You win!");
         wins ++;
-        $(".wins").text(wins);
-        randomNumberGen();
+        $(".wins").text("Wins:" + wins);
+        startGame ();
+
       }
     
       else if (counter >= targetNumber) {
         alert("You lose!!");
         losses ++;
-        $(".losses").text(losses);
-        randomNumberGen();
+        $(".losses").text("Losses:" + losses);
+        startGame ();
       }
 
+    }
+
+startGame ();
+
+
+
+$('.image-one').on("click", function(){
     
-});
+    counter = cupcakeOne + counter; 
+    $(".total-score").text(counter);
+    determineWinLose ();
+      
+}); 
+$('.image-two').on("click", function(){
+    
+    counter = cupcakeTwo+ counter; 
+    $(".total-score").text(counter);
+    determineWinLose ();
+      
+}); 
+$('.image-three').on("click", function(){
+    
+    counter = cupcakeThree + counter; 
+    $(".total-score").text(counter);
+    determineWinLose ();
+      
+}); 
+$('.image-four').on("click", function(){
+    
+    counter = cupcakeFour+ counter; 
+    $(".total-score").text(counter);
+    determineWinLose ();
+      
+}); 
+
+
+
