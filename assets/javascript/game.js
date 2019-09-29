abaab611d3f7f8e9c4291b605e706a149d33b76c
Cera Smart <cera_smart@yahoo.com>
2019-09-29 11:49:31 -0500

    finishing css touches and added some comments

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,4 @@
+// global varibles
 var counter = 0;
 var cupcakeOne =0;
 var cupcakeTwo =0;
@@ -7,14 +8,15 @@ var wins = 0;
 var losses =0;
 var targetNumber=0;
 
-
+// random number generator
 function randomNumGen (min,max){
   return Math.floor(Math.random() *(max-min +1))+min;
   
 }
+// used to start/restart game
 var startGame = function () {
     targetNumber = randomNumGen (15, 100)
-    $(".random-number").text(targetNumber);
+    $(".random-number").text("Cupcakes Needed: " + targetNumber);
     counter = 0;
     cupcakeOne = randomNumGen (1, 10)
     cupcakeTwo = randomNumGen (1, 10)
@@ -23,6 +25,7 @@ var startGame = function () {
     console.log("One:" + cupcakeOne, "Two" + cupcakeTwo, "Three" + cupcakeThree, "Four"+ cupcakeFour );
 
 }
+// used to determin if you win or  lose
 var determineWinLose = function () {
 
     if (counter === targetNumber) {
@@ -41,11 +44,11 @@ var determineWinLose = function () {
       }
 
     }
-
+//starts game when page is loaded
 startGame ();
 
 
-
+// detemines what happes when you click on the pictures
 $('.image-one').on("click", function(){
     
     counter = cupcakeOne + counter; 
